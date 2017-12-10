6ce261d259c57cdeb582d90d855d0b88aa3304b5
Mozart Louis <mozartalouis@gmail.com>
2017-12-10 02:51:24 -0500

    Completed LlrxCreator

--- JuceLibraryCode/modules/juce_core/memory/juce_LeakedObjectDetector.h
+++ JuceLibraryCode/modules/juce_core/memory/juce_LeakedObjectDetector.h
@@ -76,7 +76,7 @@ private:
         {
             if (numObjects.value > 0)
             {
-                DBG ("*** Leaked objects detected: " << numObjects.value << " instance(s) of class " << getLeakedObjectClassName());
+                /// DBG ("*** Leaked objects detected: " << numObjects.value << " instance(s) of class " << getLeakedObjectClassName());
 
                 /** If you hit this, then you've leaked one or more objects of the type specified by
                     the 'OwnerClass' template parameter - the name should have been printed by the line above.
@@ -85,7 +85,7 @@ private:
                     your object management. Tut, tut. Always, always use ScopedPointers, OwnedArrays,
                     ReferenceCountedObjects, etc, and avoid the 'delete' operator at all costs!
                 */
-                jassertfalse;
+                /// jassertfalse;
             }
         }
 
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,75 +1,32 @@
 /// Copyright (c) 2017 Mozart Alexander Louis. All rights reserved.
 
 /// Includes
-#include "../Schema/llrx_generated.h"
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "flatbuffers/util.h"
+#include "cmd/cmd.hpp"
+#include "creator/creator.h"
+#include "JuceHeader.h"
+
+/// Configure the command-line arguments for LlrxCreator
+void configure(cli::Parser &parser) {
+	parser.set_optional<std::string>("c", "config", "config.json");
+}
 
 /// Main
 int main (int argc, char* argv[]) {
-	auto file = File::getCurrentWorkingDirectory().getChildFile("test.mid");
+	std::cout << std::endl << "###### LlrxCreator - A tool to convert tailered midi files to Llrx files for Lleeria";
+	std::cout << std::endl << "###### Copyright (c) 2017 Mozart Louis" << std::endl;
 	
-	std::unique_ptr<InputStream> stream(file.createInputStream());
-	std::unique_ptr<MidiFile> midi(new MidiFile());
-	midi->readFrom(*stream);
+	/// Create command line parser to handle all the command things
+	cli::Parser parser(argc, argv);
+	configure(parser);
 	
-	std::cout << "Tracks: " << midi->getNumTracks() << std::endl;
-	midi->convertTimestampTicksToSeconds();
+	/// Read the json file and pass it to the main parser that will create llrx files. This default to config.json
+	/// but can be overwrittern with the `-c` in a terminal
+	const std::string json_file = parser.get<std::string>("c");
+	std::cout << std::endl << "###### Reading \"" << json_file << "\"..." << std::endl;
 	
-	flatbuffers::FlatBufferBuilder builder(1024);
-
-	std::vector<flatbuffers::Offset<Entity>> entities_vector;
+	/// Run the creator with the json file to create the llrx files
+	new Creator(json_file.c_str());
 	
-	for (int i = 0; i < midi->getNumTracks(); i++) {
-		auto track = midi->getTrack(i);
-		std::cout << " Track: " << i << std::endl;
-		
-		for (int j = 0; j < track->getNumEvents(); j++) {
-			auto event = track->getEventPointer(j);
-			
-			if (event->message.isNoteOn()) {
-				std::cout << "  Event: " << event->message.getDescription() << std::endl;
-				std::cout << "   Time: " << event->message.getTimeStamp() << std::endl;
-				
-				auto entity = CreateEntity(builder, event->message.getNoteNumber(), event->message.getTimeStamp(),
-																	 event->message.getChannel());
-				entities_vector.push_back(entity);
-			}
-		}
-	}
-	
-	auto entities = builder.CreateVector(entities_vector);
-	auto name = builder.CreateString("Test");
-	
-	LlrxBuilder llrxBuilder(builder);
-	llrxBuilder.add_amount(static_cast<int32_t>(entities_vector.size()));
-	llrxBuilder.add_name(name);
-	llrxBuilder.add_entities(entities);
-	
-	auto orc = llrxBuilder.Finish();
-	builder.Finish(orc);
-	
-	auto buf = builder.GetBufferPointer();
-	int size = builder.GetSize();
-	
-	flatbuffers::SaveFile("test.llrx", (const char *) buf, size, true);
-	
-	//// Test reading
-	std::string readbuf;
-	flatbuffers::LoadFile("test.llrx", true, &readbuf);
-	
-	auto readOrc = GetLlrx(readbuf.c_str());
-	std::cout << "Name: " << readOrc->name()->c_str() << std::endl;
-	std::cout << "Amount: " << readOrc->amount() << std::endl;
-	
-	for (auto it = readOrc->entities()->begin(); it != readOrc->entities()->end(); ++it) {
-		// access element as *it
-		std::cout << "Entities:" << std::endl;
-		
-		std::cout << " Note:" << it->note() << std::endl;
-		std::cout << " Time:" << it->time() << std::endl;
-		std::cout << " Delta:" << it->delta() << std::endl;
-		
-		// any code including continue, break, return
-	}
+	/// Exit
+	return 0;
 }
